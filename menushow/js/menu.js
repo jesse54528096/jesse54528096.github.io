f9061b11a88e758f8b09ea3f4d04c70682640b9b
jesse54528096 <jesse54528096@gmail.com>
2024-08-26 15:38:52 +0800

    測試網頁

---
+++ menushow/js/menu.js
@@ -0,0 +1,81 @@
+var Menu = (function() {
+
+	var $container = $( '#container' ),
+		$cover = $container.find( 'div.cover' ),
+		$middle = $container.find( 'div.middle' ),
+		$right = $container.find( 'div.right' ),
+		$open = $cover.find('div.open'),
+		$close = $right.find('span.close'),
+		$details = $container.find( 'a.viewdetails' ),
+
+		init = function() {
+
+			initEvents();
+
+		},
+		initEvents = function() {
+
+			$open.on( 'click', function( event ) {
+
+				openMenu();
+				return false;
+
+			} );
+
+			$close.on( 'click', function( event ) {
+
+				closeMenu();
+				return false;
+
+			} );
+
+			$details.on( 'click', function( event ) {
+
+				$container.removeClass( 'rm-in' ).children( 'div.rm-modal' ).remove();
+				viewDetails( $( this ) );
+				return false;
+
+			} );
+
+		},
+		openMenu = function() {
+
+			$container.addClass( 'open' );
+
+		},
+		closeMenu = function() {
+
+			$container.removeClass( 'open nodelay in' );
+
+		},
+		viewDetails = function( recipe ) {
+
+			var title = recipe.text(),
+				img = recipe.data( 'thumb' ),
+				description = recipe.parent().next().text(),
+				url = recipe.attr( 'href' );
+
+			var $modal = $( '<div class="rm-modal"><div class="rm-thumb" style="background-image: url(' + img + ')"></div><h5>' + title + '</h5><p>' + description + '</p><a href="' + url + '">See the recipe</a><span class="rm-close-modal">x</span></div>' );
+
+			$modal.appendTo( $container );
+
+			var h = $modal.outerHeight( true );
+			$modal.css( 'margin-top', -h / 2 );
+
+			setTimeout( function() {
+
+				$container.addClass( 'rm-in rm-nodelay' );
+
+				$modal.find( 'span.rm-close-modal' ).on( 'click', function() {
+
+					$container.removeClass( 'rm-in' );
+
+				} );
+
+			}, 0 );
+
+		};
+
+	return { init : init };
+
+})();
